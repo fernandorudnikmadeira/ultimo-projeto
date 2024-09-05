c72ed6903582b0d50bbb3828049df3ec86d89685
fernandorudnikmadeira <128594298+fernandorudnikmadeira@users.noreply.github.com>
2024-09-05 08:13:01 -0300

    Add files via upload

---
+++ tecnicas-computacionais-refletindo-sobre-ia-aula06/js/perguntas.js
@@ -0,0 +1,67 @@
+export const perguntas = [
+    {
+        enunciado: "O que é a trigonometria?",
+        alternativas: [
+            {
+                texto: "É o estudo das relações entre ângulos e lados em um triângulo",
+                afirmacao: ["correta!",
+                    
+                ]
+            },
+            {
+                texto: "Estudo da fauna e flora da África do Sul",
+                afirmacao: ["Incorreta",
+                    
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Aonde você pode utilizar a trigonometria?",
+        alternativas: [
+            {
+                texto: "Em situações de cálculos de distâncias inacessíveis.",
+                afirmacao: ["Correta",  
+                ]
+            },
+            {
+                texto: "Em situações de descobrimento paleontológicos.",
+                afirmacao: ["Incorreta",
+                    
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Quais são os tipos de trigonometria?",
+        alternativas: [
+            {
+                texto: "função par, função ímpar, funçao crescente, entre outros.",
+                afirmacao: ["Incorreta",
+                ]
+            },
+            {
+                texto: "Seno, Cosseno, Tangente.",
+                afirmacao: ["Correta",
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Para que serve a trigonometria na física?",
+        alternativas: [
+            {
+                texto: "Auxiliar em cáculos de calorimetria, magnetismo.",
+                afirmacao: ["Incorreta"
+                ]
+            },
+            {
+                texto: "Auxiliar em cálculos relacionados à cinemática, Dinâmica, entre outros.",
+                afirmação: ["correta"
+
+                ]   
+            }      
+                ]
+            }
+        ]
+   
